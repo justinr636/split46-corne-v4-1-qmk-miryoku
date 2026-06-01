5303f9a26de7d3d3e9b9a79f62a455a12bf873dd
Justin Ratra <justinr636@gmail.com>
2026-05-31 22:36:08 -0400

    feat: change colemak -> qwerty

---
+++ keyboards/crkbd/rev4_1/standard/keymaps/manna-harbour_miryoku/rules.mk
@@ -0,0 +1,6 @@
+# Miryoku layout options.
+# See users/manna-harbour_miryoku/post_rules.mk for the available variables
+# and users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_selection.h
+# for the available values for each variable.
+
+MIRYOKU_ALPHAS = QWERTY
