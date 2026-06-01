9a066c51e2cd3c5c0d6065ebdf3beef7db57a513
Justin Ratra <justinr636@gmail.com>
2026-05-31 22:09:29 -0400

    fix: error in compiling
    https://github.com/manna-harbour/miryoku/discussions/361

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -11,8 +11,8 @@
 #undef TAPPING_TERM
 #define TAPPING_TERM 200
 
-// Prevent normal rollover on alphas from accidentally triggering mods.
-#define IGNORE_MOD_TAP_INTERRUPT
+// IGNORE_MOD_TAP_INTERRUPT is removed in modern QMK; the equivalent
+// (do not interrupt mod-tap on rolled keypress) is now the default behaviour.
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
