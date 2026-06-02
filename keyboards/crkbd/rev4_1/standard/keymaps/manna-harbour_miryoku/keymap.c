81a8d01b5beb4a0291a3ebd2b8f0e6c0cd2f020a
Justin Ratra <justinr636@gmail.com>
2026-06-01 23:49:28 -0400

    feat: custom nav rgb

--- keyboards/crkbd/rev4_1/standard/keymaps/manna-harbour_miryoku/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/manna-harbour_miryoku/keymap.c
@@ -17,11 +17,66 @@ combo_t key_combos[] = {
 };
 
 
-// Per-layer RGB indicator: highlight the active layer's functional keys
+// Per-layer RGB indicator.
+//
+// NAV layer: per-key colors (arrows green, home/end yellow, copy/paste blue,
+// undo/redo red, NAV thumb green); every other key is unlit.
+// Other non-BASE layers: uniform highlight on the layer's functional keys.
+
+static void apply_hsv(uint8_t index, hsv_t hsv) {
+    if (hsv.v > rgb_matrix_get_val()) {
+        hsv.v = rgb_matrix_get_val();
+    }
+    rgb_t rgb = hsv_to_rgb(hsv);
+    rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
+}
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t layer = get_highest_layer(layer_state | default_layer_state);
     if (layer == U_BASE) {
+        return false;
+    }
+
+    if (layer == U_NAV) {
+        for (uint8_t i = led_min; i < led_max; i++) {
+            rgb_matrix_set_color(i, 0, 0, 0);
+        }
+
+        uint8_t base_layer = get_highest_layer(default_layer_state);
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+                if (index == NO_LED || index < led_min || index >= led_max) {
+                    continue;
+                }
+                uint16_t kc      = keymap_key_to_keycode(layer,      (keypos_t){col, row});
+                uint16_t base_kc = keymap_key_to_keycode(base_layer, (keypos_t){col, row});
+                hsv_t hsv;
+                switch (kc) {
+                    case KC_LEFT:
+                    case KC_DOWN:
+                    case KC_UP:
+                    case KC_RGHT:
+                        hsv = (hsv_t){85, 255, 255}; break;
+                    case KC_HOME:
+                    case KC_END:
+                        hsv = (hsv_t){43, 255, 255}; break;
+                    case U_CPY:
+                    case U_PST:
+                        hsv = (hsv_t){170, 255, 255}; break;
+                    case U_UND:
+                    case U_RDO:
+                        hsv = (hsv_t){0, 255, 255}; break;
+                    default:
+                        if (base_kc != LT(U_NAV, KC_SPC)) {
+                            continue;
+                        }
+                        hsv = (hsv_t){85, 255, 255};
+                        break;
+                }
+                apply_hsv(index, hsv);
+            }
+        }
         return false;
     }
 
@@ -30,7 +85,6 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
         case U_EXTRA:  hsv = (hsv_t){128, 200, 255}; break;
         case U_TAP:    hsv = (hsv_t){170, 180, 255}; break;
         case U_BUTTON: hsv = (hsv_t){21,  255, 255}; break;
-        case U_NAV:    hsv = (hsv_t){85,  255, 255}; break;
         case U_MOUSE:  hsv = (hsv_t){170, 255, 255}; break;
         case U_MEDIA:  hsv = (hsv_t){43,  255, 255}; break;
         case U_NUM:    hsv = (hsv_t){0,   255, 255}; break;
